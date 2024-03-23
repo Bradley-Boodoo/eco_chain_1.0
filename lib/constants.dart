cf99979c10a4491f704694741aeac2c9a52bff41
Bradley <bradleyjesseb@gmail.com>
2024-03-23 09:03:25 -0300

    added a color for buttons

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromARGB(150, 0, 248, 236);
 const kSecondaryColor = Color.fromARGB(232, 1, 82, 78);
 const kBackgroundColor = Color.fromARGB(255, 198, 231, 220);
-
-const kWhite = Color.fromARGB(255, 255, 255, 255);
+const kButtonColor = Color.fromARGB(136, 0, 248, 170);
